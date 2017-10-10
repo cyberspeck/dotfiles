eb0e5e9422402b02ae3b8b1453b89a4c8b8736a7
David Blacher <cyberspeck@users.noreply.github.com>
2017-10-11 00:51:42 +0200

    fixed previously removed execution of zsh_installer; cleaned up a bit

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -40,24 +40,19 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
         chsh -s $(which zsh)
+        echo " Please log out/in again for zsh to be your default shell!"
     fi
 else
     # If zsh isn't installed, get the platform of the current machine
     platform=$(uname);
     # If the platform is Linux, try an apt-get to install zsh and then recurse
     if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
-        fi
         if [[ -f /etc/debian_version ]]; then
             sudo apt-get install zsh
             install_zsh
         fi
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
     fi
 fi
 }
+
+install_zsh
