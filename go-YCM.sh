30a778942e04447f573cc02513e3d4fd6072cdf4
cyberspeck <cyberspeck@users.noreply.github.com>
2017-10-12 10:08:45 +0200

    changed git 'add submodule' to 'clone'

--- go-YCM.sh
+++ go-YCM.sh
@@ -1,7 +1,15 @@
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
-cd ~/.vim/pack/cyberspeck/start
-git submodule add https://github.com/Valloric/YouCompleteMe.git YouCompleteMe
-git submodule update --init --recursive
+
+cd vim
+mkdir pack
+cd pack
+mkdir cyberspeck
+cd cybersepck
+mkdir start
+cd start
+
+git clone https://github.com/Valloric/YouCompleteMe.git YouCompleteMe
 cd YouCompleteMe
+git submodule update --init --recursive
 ./install.py --clang-completer
