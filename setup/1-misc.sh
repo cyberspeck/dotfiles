46271ba64b330121ba8c5f2726fbfa12f44f5b3a
cyberspeck <cyberspeck@users.noreply.github.com>
2018-07-15 20:50:08 +0200

    added various 'apt install's

--- setup/1-misc.sh
+++ setup/1-misc.sh
@@ -13,8 +13,14 @@ git config --global push.default simple
 git config --global credential.helper "cache --timeout=3600"
 
 sudo apt install gnuplot gnuplot-doc
+sudo apt install xclip
+sudo apt install python-pip
+sudo apt install python-setuptools
+pip install wheel
 sudo apt install tmux
+pip install --user tmuxp
 sudo apt install paraview paraview-doc
 sudo apt install trash-cli
 sudo apt install gimp gimp-help-en gimp-help-de
 sudo apt install ffmpeg
+sudo apt install htop
