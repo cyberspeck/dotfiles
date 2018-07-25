84e656b8a4e3ea09cc0c4fc5d0860e21b84e6798
cyberspeck <cyberspeck@users.noreply.github.com>
2018-07-25 21:57:12 +0200

    added line to install mpi implementation

--- setup/4-cpp.sh
+++ setup/4-cpp.sh
@@ -9,6 +9,7 @@ sudo apt install ddd vagrind valkyrie
 # install mpicxx:
 # http://lsi.ugr.es/jmantas/pdp/ayuda/datos/instalaciones/Install_OpenMPI_en.pdf
 sudo apt install openmpi-bin libopenmpi-dev
+sudo apt-get install libcr-dev mpich mpich-doc
 # install Gnu Scientific Library:
 sudo apt install libgsl-dev
 
