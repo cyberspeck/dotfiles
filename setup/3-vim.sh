5fe6d833a94b1a884686d606af425988426ad430
cyberspeck <cyberspeck@users.noreply.github.com>
2018-07-27 11:37:49 +0200

    added vim-airline to setup scirpt

--- setup/3-vim.sh
+++ setup/3-vim.sh
@@ -25,6 +25,7 @@ git clone https://github.com/wilywampa/vim-ipython.git
 git clone https://github.com/christoomey/vim-tmux-navigator.git
 git clone https://github.com/tpope/vim-fugitive.git
 vim -u NONE -c "helptags vim-fugitive/doc" -c q
+git clone https://github.com/vim-airline/vim-airline.git
 
 git clone https://github.com/Valloric/YouCompleteMe.git YouCompleteMe
 cd YouCompleteMe
