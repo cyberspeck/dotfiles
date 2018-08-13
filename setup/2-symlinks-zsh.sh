f3a456290f13e185cf81d5f21d4a66a1b19f5cfd
cyberspeck <cyberspeck@users.noreply.github.com>
2018-08-13 10:19:06 +0200

    renamed dotfiles backup folder

--- setup/2-symlinks-zsh.sh
+++ setup/2-symlinks-zsh.sh
@@ -8,14 +8,14 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir_backup=~/dotfiles_backup      # old dotfiles backup directory
 files="bashrc vimrc vim zshrc oh-my-zsh tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+# create dotfiles_backup in homedir
+echo -n "Creating $dir_backup for backup of any existing dotfiles in ~ ..."
+mkdir -p $dir_backup
 echo "done"
 
 # change to the dotfiles directory
@@ -23,10 +23,10 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_backup directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    echo "Moving any existing dotfiles from ~ to $dir_backup"
+    mv ~/.$file $dir_backup
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
