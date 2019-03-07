cc3db531c3ae981e2a64d0ddd21842de6754049e
cyberspeck <cyberspeck@users.noreply.github.com>
2019-03-07 16:40:38 +0100

    add info on how to mount drives in wsl

--- setup/0-wsl.sh
+++ setup/0-wsl.sh
@@ -7,3 +7,10 @@ sudo apt-get install language-pack-en-base
 
 # fix for scaling issue in windows with Xming
 # http://www.straightrunning.com/xmingnotes/fonts.php#head-17
+
+# https://superuser.com/questions/1114341/windows-10-ubuntu-bash-shell-how-do-i-mount-other-windows-drives
+# mount drive in wsl:
+# sudo mkdir /mnt/d
+# sudo mount -t drvfs D: /mnt/d
+# unmount:
+# sudo umount /mnt/d
