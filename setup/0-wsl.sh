1f61df541b5114d16b571c2b209f1a3efe4cfd7e
BlacherD <david.blacher@ait.ac.at>
2019-03-08 13:54:20 +0100

    aded info on xming config for scaling issues on windows

--- setup/0-wsl.sh
+++ setup/0-wsl.sh
@@ -1,3 +1,9 @@
 # for multiple processes
 # https://github.com/Microsoft/WSL/issues/3397
 echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+
+sudo apt-get install language-pack-en-base
+# sudo dpkg-reconfigure locales
+
+# fix for scaling issue in windows with Xming
+# http://www.straightrunning.com/xmingnotes/fonts.php#head-17
