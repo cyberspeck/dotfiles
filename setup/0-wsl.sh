2e0ee9b5a827dbf9a267bb478800cb902db1896e
cyberspeck <cyberspeck@users.noreply.github.com>
2019-07-29 15:20:15 +0200

    commented language pack install

--- setup/0-wsl.sh
+++ setup/0-wsl.sh
@@ -2,7 +2,7 @@
 # https://github.com/Microsoft/WSL/issues/3397
 echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
 
-sudo apt-get install language-pack-en-base
+# sudo apt-get install language-pack-en-base
 # sudo dpkg-reconfigure locales
 
 # fix for scaling issue in windows with Xming
