3f68c8014317917df5f55cbd7674f65f404418b4
cyberspeck <cyberspeck@users.noreply.github.com>
2019-08-14 17:57:34 +0200

    change default python to 3.6

--- setup/1-sudo-installs.sh
+++ setup/1-sudo-installs.sh
@@ -3,10 +3,9 @@
 sudo apt install git
 sudo apt install zsh
 sudo apt install xclip
-sudo apt install python-pip
-sudo apt install python-setuptools
-sudo apt install python-wheel
-sudo apt install python-dev python3-dev
+sudo apt install python3-pip
+sudo apt install python3-wheel
+sudo apt install python3-dev
 sudo apt install ipython3
 sudo apt install vim-gtk
 sudo apt install neovim
@@ -19,3 +18,5 @@ sudo apt install gnuplot gnuplot-doc
 sudo apt install ffmpeg
 sudo apt install imagemagick
 
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
