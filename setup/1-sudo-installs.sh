2dec8d25addc53e0f42054dd0f709866b0fc6ce6
BlacherD <cyberspeck@users.noreply.github.com>
2019-12-12 18:42:58 +0100

    add synaptic and inkscape to installer

--- setup/1-sudo-installs.sh
+++ setup/1-sudo-installs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install git zsh xclip python3-pip python3-wheel python3-dev ipython3 vim-gtk neovim build-essential cmake tmux tmuxp htop latexmk gnuplot gnuplot-doc ffmpeg imagemagick
+sudo apt install git zsh xclip python3-pip python3-wheel python3-dev ipython3 vim-gtk neovim build-essential cmake tmux tmuxp synaptic htop latexmk gnuplot gnuplot-doc ffmpeg imagemagick inkscape
 
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
+#sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+#sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
