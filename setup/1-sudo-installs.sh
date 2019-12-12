865e1e0e5d761477c00690baf1a3204adaa7e766
BlacherD <david.blacher@ait.ac.at>
2019-12-12 14:01:03 +0100

    shorten install command

--- setup/1-sudo-installs.sh
+++ setup/1-sudo-installs.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-sudo apt install git
-sudo apt install zsh
-sudo apt install xclip
-sudo apt install python3-pip
-sudo apt install python3-wheel
-sudo apt install python3-dev
-sudo apt install ipython3
-sudo apt install vim-gtk
-sudo apt install neovim
-sudo apt install build-essential cmake
-sudo apt install tmux
-sudo apt install tmuxp
-sudo apt install htop
-sudo apt install latexmk
-sudo apt install gnuplot gnuplot-doc
-sudo apt install ffmpeg
-sudo apt install imagemagick
+sudo apt install git zsh xclip python3-pip python3-wheel python3-dev ipython3 vim-gtk neovim build-essential cmake tmux tmuxp htop latexmk gnuplot gnuplot-doc ffmpeg imagemagick
 
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
