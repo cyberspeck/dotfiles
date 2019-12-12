4d32b075478f2959168441626db8f78fa4e4d6d1
BlacherD <cyberspeck@users.noreply.github.com>
2019-12-12 18:45:53 +0100

    texstudio not installed by default

--- setup/sudo-install-tex.sh
+++ setup/sudo-install-tex.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 ############################
-# This script installs texstudio
+# This script installs tex (and optionally texstudio)
 ############################
 
-sudo add-apt-repository ppa:sunderme/texstudio
-sudo apt update
-sudo apt install texstudio
-sudo apt install texlive-base texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-fonts-recommended texlive-fonts-extra
-sudo apt install texlive-latex-recommended-doc
-sudo apt install biber texlive-bibtex-extra texlive-lang-german
+sudo apt install texlive-base texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-fonts-recommended texlive-fonts-extra texlive-latex-recommended-doc biber texlive-bibtex-extra texlive-lang-german texlive-lang-english
 
-echo -n "To finish setup: Open texstudio -> Options -> Build -> set 'Default Bibliography Tool' to 'Biber'"
-echo -n "Then, enable 'Advanced Options' and then choose 'Adv. Editor' and activate Line Numbers."
-echo -n "Optionally, change GUI scaling and font size"
-
-echo -n "Now you might want to install Jabref, too."
+echo -n "Now you might want to install Jabref or texstudio, too."
 echo -n "details on how to do that can be found in this bash script as comments."
 echo -n "or here: https://help.jabref.org/en/Installation"
+
+#sudo add-apt-repository ppa:sunderme/texstudio
+#sudo apt update
+#sudo apt install texstudio
+#echo -n "To finish setup: Open texstudio -> Options -> Build -> set 'Default Bibliography Tool' to 'Biber'"
+#echo -n "Then, enable 'Advanced Options' and then choose 'Adv. Editor' and activate Line Numbers."
+#echo -n "Optionally, change GUI scaling and font size"
 
 # check your java version:
 #java -version
