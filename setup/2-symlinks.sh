4ddf739c89d12f584635c1d20646f1c6c5c7a29f
cyberspeck <cyberspeck@users.noreply.github.com>
2020-04-29 19:10:18 +0200

    improved symlinks file

--- setup/2-symlinks.sh
+++ setup/2-symlinks.sh
@@ -18,7 +18,7 @@ echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_backup directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $dir_backup"
+    echo "Moving any existing $file from ~ to $dir_backup"
     mv ~/.$file $dir_backup
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
